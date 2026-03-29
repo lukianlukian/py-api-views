deffb65dcd9c1bcfdbc75652161454504b4e0950
Lukian <lukian55d@gmail.com>
2026-03-29 23:02:19 +0100

    flake8

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from cinema.views import MovieViewSet, GenreViewSet, ActorViewSet, CinemaHallViewSet
+from cinema.views import (
+    MovieViewSet,
+    GenreViewSet,
+    ActorViewSet,
+    CinemaHallViewSet,
+)
 
 router = DefaultRouter()
 router.register("movies", MovieViewSet, basename="movie")
